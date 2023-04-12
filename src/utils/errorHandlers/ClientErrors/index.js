efe3c9a9ff918dda1a94c6e3023446c8d42ea03c
Aditya Mukherjee <adimukh2001@gmail.com>
2023-04-12 16:54:13 +0530

    Added graceful Error handlers

---
+++ src/utils/errorHandlers/ClientErrors/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    PasswordMismatchError: require('./password-mismatch'),
+    UserNotFoundError: require('./user-not-found-error'),
+    ValidationError: require('./validation-error'),
+    TokenVerificationError: require('./token-verification-error'),
+    DuplicateEntryError: require('./duplicate-entry')
+};
